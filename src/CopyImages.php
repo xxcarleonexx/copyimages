59525eefeeb5410ea783cf6816b196bb92020808
carleone <xxcarleonexx@mail.ru>
2016-01-05 19:41:36 +0200

    Final path

--- src/CopyImages.php
+++ src/CopyImages.php
@@ -1,134 +1,139 @@
 <?php
-    namespace CopyImages;
+/**
+* CopyImages служит простым парсером удаленного хоста.
+* 
+* Объект парсит удаленный хост, копируя картинки на компьютер, где запущен парсер.
+* Позволяет сохранять изображения с расширением *.jpg, *.png, *.gif.
+*
+* @author Sergey Rusanov
+* @version 0.0.1-dev
+*/
+namespace CopyImages;
     
-    use Exception;
-    use DOMDocument;
-    use DOMXPath;
+use Exception;
+use DOMDocument;
+use DOMXPath;
     
+class CopyImages
+{
     /**
-     function stream_copy($src, $dest) 
-     { 
-         $fsrc = fopen($src,'r'); 
-         $fdest = fopen($dest,'w+'); 
-         $len = stream_copy_to_stream($fsrc, $fdest); 
-         fclose($fsrc); 
-         fclose($fdest); 
-         return $len; 
-     }
+    * @var address Адрес удаленного хоста, откуда будут копироваться изображения.
+    * @var ingTypes Массив типов изображений, которые будут искаться на хосте.
+    * @var ddir Название директории, куда будут копироваться файлы.
     */
-    
-    class CopyImages
+    private $address;
+    private $imgTypes;
+    private $ddir;
+        
+    function __construct($addr)
     {
-        private $address;
-        private $imgTypes;
-        private $ddir;
-        
-        function __construct($addr)
-        {
-            if(empty($addr)) throw new Exception(
-                sprintf("Please set host address."),
-                1
-                );
-            $this->address = urldecode($addr);
-            $this->imgTypes = ['jpg', 'png', 'gif'];
-            $this->ddir = "downloaded";
-        }
-        
-        public function setAddress($addr)
-        {
-            $this->address = $addr;
-        }
-        
-        /*
-        @return : возвращает адрес сайта установленого раннее.
-        */
-        public function getAddress()
-        {
-            return $this->address;
-        }
-        
-        /*
-        @return : возвращает true всегда.
-        */
-        public function getTrue()
-        {
-            return true;
-        }
-        
-        /*
-        @return : возвращает true в случае успешного копирования.
-            Иначе Exception в случае исключения. Или false если файлов не оказалось.
-        */
-        public function copyFiles()
-        {
-            if(is_dir($this->ddir)) 
-                chdir($this->ddir);
-            else {
-                if(!mkdir($this->ddir, 0777))
-                throw new Exception(
-                    sprintf("Can't create directory %s with mode = %o", $this->ddir, 0777),
-                    2
-                );
-                else {
-                    chdir($this->ddir);
-                    if(false === ($foundArr = $this->parseFromUrl())) return false;
-                    foreach($foundArr as $elem) 
-                    {
-                        
-                    }
-                    /*
-                    $resFileFrom = fopen($fileFrom, 'r');
-                    $resFileTo = fopen($fileTo, 'w+');
-                    */
-                    }
-            }
-            return true;
-        }
-        
-        /*
-        @return : возвращает массив адресов файлов картинок.
-                Или false если файлов удовлетворяющих критерию не оказалось.
-        */
-        public function parseFromUrl()
-        {
-            $addr = $this->address;
-            if(!$curl = curl_init($addr)) throw new Exception(
-                    sprintf("Can't initialize cUrl. Please install or enable this extention in php."),
-                    3
-                );
-            
-            curl_setopt($curl, CURLOPT_AUTOREFERER, true);
-            curl_setopt($curl, CURLOPT_COOKIESESSION, true);
-            curl_setopt($curl, CURLOPT_HEADER, true);
-            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-            curl_setopt($curl, CURLOPT_VERBOSE, false);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101");
-            
-            if(false === ($execCurl = curl_exec($curl))) throw new Exception(
-                    sprintf("Error during exec cUrl query."),
-                    4
-                );
-            curl_close($curl);
-            
-            $dom = new DOMDocument();
-            @$dom->loadHTML($execCurl);
-            
-            $xpath = new DOMXPath($dom);
-            $xpath = $xpath->query(sprintf(".//img[contains(@src, '.%s')]", implode("') or contains(@src, '.",$this->imgTypes)));
-            if($xpath)
-            {
-                $found = [];
-                foreach($xpath as $entry)
-                    {
-                        $src = $entry->getAttribute('src');
-                        if(strcmp($src, "") !== true)
-                        {
-                            $found[] = $src;
-                        };
-                    }
-                return $found;
-            }
-            return false;
-        }
+        if (empty($addr)) throw new Exception(sprintf("Please set host address."), 1);
+        $this->address = urldecode($addr);
+        $this->imgTypes = ['jpg', 'png', 'gif'];
+        $this->ddir = "downloaded";
     }
+
+    /**
+    *@return : возвращает true в случае успешного копирования.
+    *          Exception в случае исключения. Или false если файлов не оказалось.
+    */
+    public function copyFiles()
+    {
+        //Запускаем парсинг хоста и проверяем наличие изображений.
+        if (false === ($foundArr = $this->parseFromUrl())) return false;
+        //Ищем нужную папку. Если не существует, то создаем её.
+        //Игнорируем выдачу предупреждений.
+        if (is_dir($this->ddir)) {
+                if (@chdir($this->ddir) === false) {
+                    throw new Exception(sprintf("Can't change directory %s", $this->ddir), 5);
+                }
+        } else {
+            if (@mkdir($this->ddir, 0777) === false) {
+                throw new Exception(sprintf("Can't create directory %s with mode = %o", $this->ddir, 0777), 2);
+            } else {
+                if (@chdir($this->ddir) === false) {
+                    throw new Exception(sprintf("Can't change directory %s", $this->ddir), 5);
+                }
+            }
+        }
+        //Задаем маску для поиска правильно заданного адреса.
+        $regMask = "~(?:(?:ftp|https?)?://|www\.)[a-z_.]+?[a-z_]{2,6}(:?/[a-z0-9\-?\[\]=&;#]+)?~i";
+        foreach($foundArr as $elem) {
+            //Проверяем есть ли в адресе полный путь к файлу.
+            //Если нет, пытаемся добавить адрес хоста.
+            $fileFrom = preg_match($regMask, $elem) ? $elem : $this->address . "/" . $elem;
+            $baseName = urldecode(pathinfo($fileFrom)['basename']);
+            //Проверяем кодировку для файловой системы ОС.
+            $fileTo = substr(PHP_OS, 0, 3) === "WIN" ? mb_convert_encoding($baseName, "Windows-1251", "UTF-8") : $baseName;
+            //Проверяем соответсвия расширений найденых файлов, заданых в конструкторе.
+            if (array_search(pathinfo($fileTo)['extension'], $this->imgTypes) === false) continue;
+                $resFileFrom = fopen($fileFrom, 'r');
+                $resFileTo = fopen($fileTo, 'w+');
+                //Копируем файлы.
+                stream_copy_to_stream($resFileFrom, $resFileTo);
+                //Отдаем занятые ресурсы.
+                fclose($resFileFrom);
+                fclose($resFileTo);
+        }
+        //Возвращаемся на уровень вверх.
+        //Поскольку изменили текущий каталог на 'downloaded'
+        chdir("../");
+        return true;
+    }
+
+    /**
+    *@return : Установленный адрес хоста. 
+    */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+    *@return : возвращает массив адресов файлов картинок.
+    *          Или false если файлов удовлетворяющих критерию не оказалось.
+    */
+    public function parseFromUrl()
+    {
+        //Инициализируем cURl для работы с хостом.
+        $addr = $this->address;
+        if (false === ($curl = curl_init($addr))) {
+            throw new Exception(sprintf("Can't initialize cUrl. Please install or enable this extention in php."), 3);
+        }
+        //Настраиваем cUrl для запроса.
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+        curl_setopt($curl, CURLOPT_COOKIESESSION, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_VERBOSE, false);
+        curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101");
+            
+        if (false === ($execCurl = curl_exec($curl))) {
+            throw new Exception(sprintf("Error during exec cUrl query."), 4);
+        }
+        //Освобождаем ресурс.
+        curl_close($curl);
+            
+        $dom = new DOMDocument();
+        //Загружем DOMDocument. Игнорируем предупреждения.
+        @$dom->loadHTML($execCurl);
+        //Создаем объект DOMXPath для XPath запроса к нему.
+        //Поиск осуществляем по критерию расширения файла.
+        $xpath = new DOMXPath($dom);
+        $xpath = $xpath->query(sprintf(".//img[contains(@src, '.%s')]", implode("') or contains(@src, '.",$this->imgTypes)));
+        //Проверка наличия картинок на хосте.
+        if ($xpath) {
+            $found = [];
+            foreach($xpath as $entry) {
+                //Проверяем наличие аттрибута src и он не пуст.
+                $src = $entry->getAttribute('src');
+                if (strcmp($src, "") !== true) {
+                    $found[] = $src;
+                };
+            }
+            return $found;
+        }
+        return false;
+    }
+}
--- test/CopyImagesTest.php
+++ test/CopyImagesTest.php
@@ -1,48 +1,116 @@
 <?php
-    
-    namespace CopyImages;
-    
-    use PHPUnit_Framework_TestCase;
-    
-    class CopyImagesTest extends PHPUnit_Framework_TestCase 
+/**
+* CopyImagesTest служит тестом для CopyImages.
+*
+* @author Sergey Rusanov
+* @version 0.0.1-dev
+*/
+namespace CopyImages;
+
+class CopyImagesTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+    * Тестируем, что директории нет. Или если она есть, то ее удаляем.
+    */
+    public function setUp()
     {
-        
-        public function testCreateObj()
-        {
-            $path = "www.ya.ru";
-            $copyimg = new CopyImages($path);
-            $this->assertEquals($path, $copyimg->getAddress());
-        }
-        
-        /**
-        * @expectedException Exception
-        * @expectedExceptionMessage Please set host address
-        */
-        public function testExceptionHasRightMessage()
-        {
-            $copyimg = new CopyImages("");
-        }
-        
-        public function testParseFromUrl()
-        {
-            $path = "http://www.w3.org/";
-            $expected = [
-                "/2008/site/images/header-link.gif",
-                "/2008/site/images/header-link.gif",
-                "/2008/site/images/header-link.gif",
-                "/2008/site/images/header-link.gif",
-                "/2008/site/images/header-link.gif",
-                "/2008/site/images/header-link.gif",
-                "/2008/site/images/wplogo_transparent.png",
-                "/2015/04/w3cx-home.png",
-                "/2008/site/images/w3devcampus.png",
-                "/2008/site/images/ttwf-dinos.png",
-                "/2008/site/images/header-link.gif",
-                "/2008/site/images/header-link.gif",
-                "/2008/site/images/header-link.gif",
-                "/2014/10/stdvidthumb.png"
-            ];
-            $copyimg = new CopyImages($path);
-            $this->assertEquals($expected, $copyimg->parseFromUrl());
+        $dirName = "downloaded";
+        if(is_dir($dirName)) {
+            $dir = dir($dirName);
+            while(false !== ($entry = $dir->read())) {
+                if ($entry === "." || $entry === "..") continue;
+                if (file_exists($dirName . DIRECTORY_SEPARATOR . $entry)) {
+                    @unlink($dirName . DIRECTORY_SEPARATOR . $entry);
+                }
+            }
+            $dir->close();
+            rmdir($dirName);
         }
     }
+
+    /**
+    * Тестируем успешность создания объекта.
+    */
+    public function testCreateObj()
+    {
+        $path = "www.ya.ru";
+        $copyimg = new CopyImages($path);
+        $this->assertEquals($path, $copyimg->getAddress());
+    }
+
+    /**
+    * Тестируем правильность возникновения исключения.
+    * @expectedException Exception
+    * @expectedExceptionMessage Please set host address
+    */
+    public function testExceptionHasRightMessage()
+    {
+        $copyimg = new CopyImages("");
+    }
+
+    /**
+    * Тестируем правильность нахождения изображений на хосте.
+    */
+    public function testParseFromUrl()
+    {
+        $path = "http://www.w3.org/";
+        $expected = [
+            "/2008/site/images/header-link.gif",
+            "/2008/site/images/header-link.gif",
+            "/2008/site/images/header-link.gif",
+            "/2008/site/images/header-link.gif",
+            "/2008/site/images/header-link.gif",
+            "/2008/site/images/header-link.gif",
+            "/2008/site/images/wplogo_transparent.png",
+            "/2015/04/w3cx-home.png",
+            "/2008/site/images/w3devcampus.png",
+            "/2008/site/images/ttwf-dinos.png",
+            "/2008/site/images/header-link.gif",
+            "/2008/site/images/header-link.gif",
+            "/2008/site/images/header-link.gif",
+            "/2014/10/stdvidthumb.png"
+        ];
+        $copyimg = new CopyImages($path);
+        $this->assertEquals($expected, $copyimg->parseFromUrl());
+    }
+
+    /**
+    * Тестируем какие именно файлы скопированы.
+    */
+    public function testCopyFiles()
+    {
+        $path = "http://www.w3.org/";
+        $expected = [
+            ".",
+            "..",
+            "header-link.gif",
+            "stdvidthumb.png",
+            "ttwf-dinos.png",
+            "w3cx-home.png",
+            "w3devcampus.png",
+            "wplogo_transparent.png"
+        ];
+        $copyimg = new CopyImages($path);
+        $copyimg->copyFiles();
+        $this->assertEquals($expected, scandir("downloaded"));
+    }
+
+    /**
+    * Очищаем после работы тестировщика.
+    */
+     public function tearDown()
+    {
+        $dirName = "downloaded";
+        if(is_dir($dirName)) {
+            $dir = dir($dirName);
+            while(false !== ($entry = $dir->read())) {
+                if ($entry === "." || $entry === "..") continue;
+                if (file_exists($dirName . DIRECTORY_SEPARATOR . $entry)) {
+                    @unlink($dirName . DIRECTORY_SEPARATOR . $entry);
+                }
+            }
+            $dir->close();
+            rmdir($dirName);
+        }
+    }
+}
